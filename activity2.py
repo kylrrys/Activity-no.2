aa8c6292b1542f9055a249d24ff28ed61662efa2
Kyle Reyes <kylrrys@gmail.com>
2021-09-21 06:20:57 +0000

    third

--- activity2.py
+++ activity2.py
@@ -8,7 +8,26 @@ print("")
 print("Average Grade is =",str(av))
 print("")
 print("")
-if (av>=75):
-    print("The Student passed")
-else:
-    print("The Student failed")
+
+if (av<=60):
+	print(" STUDENT RATING F ")
+	print(" FAILED!!! ")
+elif (av>=61 and av<=70):
+	print(" STUDENT RATING E ")
+	print(" FAILED!!! ")
+elif (av>=71 and av<=79):
+	if av>=75:
+	    print(" STUDENT RATING D ")
+	    print(" PASSED!! ")
+	else:
+	    print(" STUDENT RATING D ")
+	    print(" FAILED!! ")
+elif (av>=81 and av<=89):
+	print(" STUDENT RATING C")
+	print(" PASSED!!! ")
+elif (av>=90 and av<=98):
+	print(" STUDENT RATING B ")
+	print(" PASSED!!! ")
+elif (av>=99 and av<=100):
+	print(" STUDENT RATING A ")
+	print(" PASSED!!! ")
