5840e7ad59031eeaeb767c76d2f164d0746e4330
Kyle Reyes <kylrrys@gmail.com>
2021-09-21 05:40:48 +0000

    for the ThirdPart

--- activity2.py
+++ activity2.py
@@ -8,4 +8,16 @@ print("")
 print("Average Grade is =",str(av))
 print("")
 print("")
+if (av<=60):
+    print("Grade Rating is F")
+elif (av>=61 and av<=70):
+    print("Grade Rating is E")
+elif (av>=71 and av<=79):
+    print("Grade Rating is D")
+elif (av>=80 and av<=89):
+    print("Grade Rating is C")
+elif (av>=90 and av<=98):
+    print("Grade Rating is B")
+else:
+    print("Grade Rating is A")
 
